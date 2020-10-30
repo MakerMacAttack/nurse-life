c1f1e5c2b1f4c40423ee4771601dd028127c19a7
eeds90 <eddie.diaz90@icloud.com>
2020-10-30 14:16:40 -0500

    Edit button done

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Post.css";
-
+import { Link } from "react-router-dom"
 
 const Post = (props) => {
   return (
     <>
-        <h2 className="post-name">{props.name}</h2>
-        <h3 className="post-content">{props.content}</h3>
-        <h4 className="post-imgURL">{props.imgURL}</h4>
+      <h2 className="post-name">{props.name}</h2>
+      <h3 className="post-content">{props.content}</h3>
+      <h4 className="post-imgURL">{props.imgURL}</h4>
+      <div>
+        <button className="edit-button">
+          <Link className="edit-link" to={`/posts/${props._id}/edit`}>
+            Edit
+          </Link>
+        </button>
+      </div>
     </>
   );
 };
--- client/src/components/shared/Nav/Nav.jsx
+++ client/src/components/shared/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./shared/Nav/Nav.css";
+import "./shared/Nav.css";
 import { NavLink } from "react-router-dom";
 
 
