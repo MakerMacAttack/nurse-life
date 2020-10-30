6c0261a1864d631eade3fcc5549b307922ecfac0
Mac May <tmcpbmay@gmail.com>
2020-10-30 09:44:09 -0400

    set up postlist

--- client/src/components/PostList/PostList.jsx
+++ client/src/components/PostList/PostList.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Post from "../Post/Post";
 
 export default function PostList(props) {
-  return <div></div>;
+  return (
+    <div id="post-list">
+      {props.posts.map((post) => (
+        <Post
+          key={post._id}
+          name={post.name}
+          content={post.content}
+          imgURL={post.imgURL}
+        />
+      ))}
+    </div>
+  );
 }
