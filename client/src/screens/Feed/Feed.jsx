f497facdfd6c44a79222e87cb8b189996f0a5e75
Mac May <tmcpbmay@gmail.com>
2020-10-30 10:40:37 -0400

    add basic feed

--- client/src/screens/Feed/Feed.jsx
+++ client/src/screens/Feed/Feed.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import PostCreate from "../../components/PostCreate/PostCreate"
+import PostList from "../../components/PostList/PostList"
 
 export default function Feed(props) {
-  return <div></div>;
+  const posts = {
+    name = "mac",
+    content = "first post",
+    imgURL = "https://developer.nvidia.com/sites/default/files/akamai/embedded/images/2018-power-up-logo-promo.jpg"
+  }
+
+  return (
+    <div>
+      <PostCreate />
+      <PostList posts={posts} />
+    </div>
+  );
 }
