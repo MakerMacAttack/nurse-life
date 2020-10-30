1a674d0c7969052b35a34b4da0652a220d4dc731
Nick Leonidov <nickleonidov@Nicks-MacBook-Air.local>
2020-10-30 10:58:09 -0400

    Feed instead of getPosts

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,11 @@ import React from "react";
 import "./App.css";
 import Feed from "./screens/Feed/Feed";
 import { Route } from "react-router-dom";
-import { getPosts } from "./services/Posts";
 
 const App = () => {
   return (
     <div className="App">
-      <Route exact path="/" component={getPosts} />
+      <Route exact path="/" component={Feed} />
     </div>
   );
 };
