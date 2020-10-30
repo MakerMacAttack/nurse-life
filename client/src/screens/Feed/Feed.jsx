f0615f42dba27324cdc595cb2eeaad22a3b8facc
Mac May <tmcpbmay@gmail.com>
2020-10-30 10:45:51 -0400

    fix hardcode post

--- client/src/screens/Feed/Feed.jsx
+++ client/src/screens/Feed/Feed.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import PostCreate from "../../components/PostCreate/PostCreate"
-import PostList from "../../components/PostList/PostList"
+import PostCreate from "../../components/PostCreate/PostCreate";
+import PostList from "../../components/PostList/PostList";
 
 export default function Feed(props) {
   const posts = {
-    name = "mac",
-    content = "first post",
-    imgURL = "https://developer.nvidia.com/sites/default/files/akamai/embedded/images/2018-power-up-logo-promo.jpg"
-  }
+    name: "mac",
+    content: "first post",
+    imgURL:
+      "https://developer.nvidia.com/sites/default/files/akamai/embedded/images/2018-power-up-logo-promo.jpg",
+  };
 
   return (
     <div>
