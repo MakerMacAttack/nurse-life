69ccda5669c118078cd0594ded1edc04f0ee223d
eeds90 <eddie.diaz90@icloud.com>
2020-10-30 09:24:34 -0500

    update to Post component

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import "./Post.css";
-import { Link } from "react-router-dom";
+
 
 const Post = (props) => {
   return (
     <>
-      <Link className="post" to={`/posts/${props._id}`}>
         <h2 className="post-name">{props.name}</h2>
         <h3 className="post-content">{props.content}</h3>
         <h4 className="post-imgURL">{props.imgURL}</h4>
-      </Link>
     </>
   );
 };
