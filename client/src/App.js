a629a5f5ae6ec3f3930d0a9ce80049f2764c0e1b
Mac May <tmcpbmay@gmail.com>
2020-11-02 12:17:38 -0500

    begin work on the static profile pae

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Feed from "./screens/Feed/Feed";
+import Profile from "./screens/Profile/Profile";
 import Header from "./components/shared/Header/Header";
 import { Route, Switch } from "react-router-dom";
 import PostEdit from "./components/PostEdit/PostEdit";
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path="/" component={Feed} />
         <Route path="/add-post" component={PostCreate} />
         <Route exact path="/posts/:id/edit" component={PostEdit} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </div>
   );
