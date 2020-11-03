23cc9a33f11015cd5721c74e40e29f5e2abfa9d0
Kashiff Khan <kashiff.k@yahoo.com>
2020-11-03 17:50:29 -0500

    Header changes

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -6,18 +6,24 @@ const Header = () => {
     <div>
       <div className="header">
           <div className="nav-top">
-            <span className="signInLink">Sign In</span>
+            <span className="signInLink">Sign Out</span>
           </div>
           <div className="nav-bottom">
             <div className="nav-bottom-left">
               <img className="logo" src="./assets/images/Main_Logo.png" alt="nurseLine"></img>
               <input className="searchBar" placeholder="Search Bar..."></input>
             </div>
-            <div className="nav-bottom-right">
+          <div className="nav-bottom-right">
+            <div className="accounts-div">
               <img className="account" src="./assets/images/account_nav.png" alt="account"></img>
+              <span className="accountLink">Account</span>
+            </div>
+            <div className="favorites-div">
               <img className="favorites" src="./assets/images/nav_bar_favorites.png" alt="favorites"></img>
+              <span className="favoritesLink">Favorites</span>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
