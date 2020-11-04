765fd73ae8200d61c43519aad4734c170d06aac3
eeds90 <eddie.diaz90@icloud.com>
2020-11-04 09:17:20 -0600

    My Account CSS

--- client/src/screens/Profile/Profile.jsx
+++ client/src/screens/Profile/Profile.jsx
@@ -4,14 +4,14 @@ import "./Profile.css";
 export default function Profile(props) {
   return (
     <div>
-      <div id="profile-header">
-        <div id="pic-box">
+      <div className="profile-header">
+        <div className="pic-box">
           <img
-            id="profile-pic"
+            className="profile-pic"
             src="./assets/images/ProfilePic.png"
             alt="User profile picture"
           />
-          <p id="edit-pic">Edit Profile Picture</p>
+          <p className="edit-pic">Edit Profile Picture</p>
         </div>
         <h1>My Account</h1>
       </div>
@@ -20,15 +20,22 @@ export default function Profile(props) {
         <button type="submit" className="save-profile-button">
           Save
         </button>
-        <div id="profile-updater">
-          <h2>Name</h2>
-          <div id="profile-name-box">
-            <label htmlFor="first">First Name</label>
-            <input id="first" name="first" placeholder="First Name" />
-            <label htmlFor="last">Last Name</label>
-            <input id="last" name="last" placeholder="Last Name" />
+        <div className="profile-updater">
+          <div className="Name">
+            <h2>Name</h2>
           </div>
-          <div id="profile-gender-box">
+          <div className="profile-name-box">
+            <label htmlFor="first"></label>
+            <input className="first" name="first" />
+            <label htmlFor="last"></label>
+            <input className="last" name="last" />
+          </div>
+            <div className="First-Last-Name">
+              <p> First Name </p> <p> Last Name </p>
+            </div>
+          
+
+          <div className="profile-gender-box">
             <label htmlFor="gender">Gender</label>
             <select id="gender" name="gender">
               <option value="male">Male</option>
@@ -37,11 +44,12 @@ export default function Profile(props) {
               <option value="not-say">Prefer not to say</option>
             </select>
           </div>
-          <div id="profile-bday-box">
+
+          <div className="profile-bday-box">
             <label htmlFor="birthday">Date of Birth</label>
             <input type="date" id="birthday" name="birthday" />
           </div>
-          <div id="profile-work-box">
+          <div className="profile-work-box">
             <label htmlFor="workplace">Institution/Work Place</label>
             <input
               id="workplace"
@@ -53,7 +61,8 @@ export default function Profile(props) {
             <label htmlFor="state">State</label>
             <input id="state" name="state" placeholder="State" />
           </div>
-          <div id="profile-contact-box">
+
+          <div className="profile-contact-box">
             <label htmlFor="phone">Phone Number</label>
             <input
               id="phone"
