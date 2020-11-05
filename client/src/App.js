5821464cc89d0df0bcc779e166a8774fe00b93b2
Mac May <tmcpbmay@gmail.com>
2020-11-05 12:13:41 -0500

    able to log in

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,9 @@ import PostEdit from "./components/PostEdit/PostEdit";
 import PostCreate from "./components/PostCreate/PostCreate";
 import SignIn from "./components/SignIn/SignIn";
 import Privacy from "./screens/Privacy/Privacy";
-import { getUser } from './services/Users'
 
 const App = () => {
-  const [user, getUser] = useState(null)
+  const [user, setUser] = useState(null)
 
   const history = useHistory()
 
@@ -27,7 +26,12 @@ const App = () => {
         <Route path="/add-post" component={PostCreate} />
         <Route exact path="/posts/:id/edit" component={PostEdit} />
         <Route path="/profile" component={Profile} />
-        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-in"
+          render={() => (
+            <SignIn setUser={setUser} />
+          )}
+        // component={SignIn} setUser={setUser}
+        />
         <Route path="/privacy" component={Privacy} />
       </Switch>
     </div>
