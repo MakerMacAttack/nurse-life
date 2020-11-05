db1836e5b314ed03add8321bb16fc3dc0fab785f
Mac May <tmcpbmay@gmail.com>
2020-11-05 15:07:19 -0500

    seeding properly, still debugging execution

--- seed/posts.js
+++ seed/posts.js
@@ -1,17 +1,47 @@
-import { getUsers } from '../client/src/services/Users'
-
 const db = require('../db/connection')
 const Post = require('../models/posts')
-
-const testUser = getUsers()
+const User = require('../models/users')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
+  const user1 = new User({
+    name:
+    {
+      first: "Ted",
+      last: "Baxter",
+    },
+    gender: "Female",
+    password: "nimrod",
+    birthday:
+    {
+      year: 1969,
+      month: 08,
+      day: 05,
+    },
+    work:
+    {
+      institution: "St. Elsewhere",
+      state: "Michigan",
+      city: "Ypsilanti",
+    },
+    contact:
+    {
+      phone: "806-454-6666",
+      email: "name@site.com"
+    },
+    privacy:
+    {
+      anonymous: false,
+      locationServices: false,
+    }
+  })
+      await user1.save()
+  
     const posts = 
     [
       {
-        user: testUser[0],
+        user: user1,
         content: "first post",
         imgURL:
           "https://developer.nvidia.com/sites/default/files/akamai/embedded/images/2018-power-up-logo-promo.jpg",
