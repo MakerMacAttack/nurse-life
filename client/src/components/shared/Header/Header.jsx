89aed0a420e69cf7aef442d829b78bdf2d0dbcde
Nick Leonidov <nickleonidov@Nicks-MacBook-Air.local>
2020-11-06 12:29:09 -0500

    Fixed h5 on signIn and adjusted header links to be centered

--- client/src/components/shared/Header/Header.jsx
+++ client/src/components/shared/Header/Header.jsx
@@ -5,30 +5,34 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <div className="header">
-          <div className="nav-top">
+        <div className="header">
+            <div className="nav-top">
             <span className="signInLink">Sign Out</span>
-          </div>
-          <div className="nav-bottom">
-            <div className="nav-bottom-left">
-              <img className="logo" src="./assets/images/Main_Logo.png" alt="nurseLine"></img>
-              <input className="searchBar" placeholder="Search Bar..."></input>
             </div>
-          <div className="nav-bottom-right">
-            <Link to="/profile">
+            <div className="nav-bottom">
+              <div className="nav-bottom-left">
+                <img className="logo" src="./assets/images/Main_Logo.png" alt="nurseLine"></img>
+                <input className="searchBar" placeholder="Search Bar..."></input>
+              </div>
+            <div className="nav-bottom-right">
+              <Link to="/profile">
               <div className="accounts-div">
                 <img className="account" src="./assets/images/account_nav.png" alt="account"></img>
+              </div>
+              <div className="phraseLink1">
                 <span className="accountLink">Account</span>
               </div>
-            </Link>
-            <div className="favorites-div">
-              <img className="favorites" src="./assets/images/nav_bar_favorites.png" alt="favorites"></img>
-              <span className="favoritesLink">Favorites</span>
+              </Link>
+              <div className="favorites-div">
+                <img className="favorites" src="./assets/images/nav_bar_favorites.png" alt="favorites"></img>
+              </div>
+              <div className="favoritesLink">
+                <span className="favoritesLink">Favorites</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
