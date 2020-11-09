df61e493f297b6fbe72cf35e9709977156f668b3
Kashiff Khan <kashiff.k@yahoo.com>
2020-11-09 13:51:12 -0500

    sign in page and footer update

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <h5>Copyright © 2020 Team IV. All Rights Reserved</h5>
+      <h5 className="copyrightInfo">Copyright © 2020 Team IV. All Rights Reserved</h5>
       <h6>Accessibility | Privacy | Legal | Sitemap</h6>
     </div>
   )
